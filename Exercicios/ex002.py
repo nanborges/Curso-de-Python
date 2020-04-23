df6a113b403146c45bfb38b58ffbb7672a2b51ee
nanborges <nan_borges@yahoo.com.br>
2020-04-23 20:40:51 -0300

    Print com input

---
+++ Exercicios/ex002.py
@@ -0,0 +1,5 @@
+#Exercicio 002
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+
+nome = input("Qual é o seu nome? ")
+print('Prazer te conhecer, {}!'.format(nome)) #{} vai ser substituido pela saida
