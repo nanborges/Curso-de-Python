13da24e9ed0589cf8601241012697309fe0984af
nanborges <nan_borges@yahoo.com.br>
2020-04-23 20:41:12 -0300

    Soma de dois números

---
+++ Exercicios/ex003.py
@@ -0,0 +1,7 @@
+#Exercicio 003
+#Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+print('A soma de {} e {} é igual a {} '.format(n1,n2,soma))
