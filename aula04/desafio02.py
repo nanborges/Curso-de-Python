ddc00f31fa94ec7ede3ba6d116e5fa9f49c0e125
nanborges <nan_borges@yahoo.com.br>
2020-04-23 19:32:39 -0300

    Data de nascimento

--- aula04/desafio02.py
+++ aula04/desafio02.py
@@ -1,5 +1,5 @@
 print('Qual a sua data de nascimento?')
 dia = input('Dia: ')
-mes = input('Mes (por extenso): ')
+mes = input('Mes: ')
 ano = input('Ano: ')
 print('Voce nasceu dia ', dia, 'de ', mes, 'de ', ano, ', correto?')
